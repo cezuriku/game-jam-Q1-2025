b8be4ac0cf542e54fea623b52192681c4b95cd83
cezuriku <cezuriku@users.noreply.github.com>
2025-04-08 18:30:59 +0200

    Initial commit

---
+++ plug.gd
@@ -0,0 +1,22 @@
+extends "res://addons/gd-plug/plug.gd"
+
+
+func _plugging():
+	plug(
+		"godot-extended-libraries/godot-debug-menu",
+		{"commit": "0e5f15217285c76170039c9cefcf79c8ab0ec6b3", "renovate-branch": "master"}
+	)
+	plug(
+		"KoBeWi/Godot-Universal-Fade",
+		{"commit": "f091514bba652880f81c5bc8809e0ee4498988ea", "renovate-branch": "master"}
+	)
+
+	# Patched version
+	plug(
+		"florianvazelle/EasyMenus",
+		{"commit": "018ab39001f862abbd6ca424258a9a548589d61c", "renovate-branch": "master"}
+	)
+	plug(
+		"florianvazelle/Log",
+		{"commit": "f23215571cb398c16272227130885d867d12cb4b", "renovate-branch": "main"}
+	)
